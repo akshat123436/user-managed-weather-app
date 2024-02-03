b12c7a14f2ec66388fc19826e72c1c4b0cc867fb
akshat123436 <akshat123436@gmail.com>
2024-02-03 15:46:17 +0530

    initial commit

---
+++ alpaago/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAttiR2tHaJldabmhzPo7wguPyy8EUnW0g",
+  authDomain: "user-managed-weather-app.firebaseapp.com",
+  projectId: "user-managed-weather-app",
+  storageBucket: "user-managed-weather-app.appspot.com",
+  messagingSenderId: "14568489966",
+  appId: "1:14568489966:web:f360497277617645ac84ff",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
